0ba773d7a96b4583fb5eb358d9cb1362384ce226
Axel <axelomatthew.terang@gmail.com>
2025-03-13 21:53:27 +0700

    feat:praktikum-5

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
@@ -34,5 +35,6 @@ Route::prefix('users')->group(function(){
 });
 
 Route::get('/level', [LevelController::class,'index']);
+Route::get('/kategori',[KategoriController::class, 'index']);
 
 
