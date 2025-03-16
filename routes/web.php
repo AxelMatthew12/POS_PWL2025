bbfd39f63bd302008e39fa8a585b0862b7208727
Axel <axelomatthew.terang@gmail.com>
2025-03-16 20:35:16 +0700

    feat:js_04Praktikum-2.6-2.7

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,10 @@ Route::get('/kategori',[KategoriController::class, 'index']);
 
 Route::get('/user', [UserController::class, 'index']);
 
+// Praktikum 2.6
+Route::get('/user/tambah',[UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan',[UserController::class, 'tambah_simpan']);
+
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
