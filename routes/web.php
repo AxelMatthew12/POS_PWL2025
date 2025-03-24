244c77f9d4e8cf2c644524d34f5e31163daf93c2
Axel <axelomatthew.terang@gmail.com>
2025-03-24 20:37:29 +0700

    feat:JS_05Praktikum-3dan4

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::prefix('products')->group(function(){
-    Route::get('/category/food-beverage',[CategoryController::class,'foodBeverage']);
-    Route::get('/category/beauty-health',[CategoryController::class,'beautyHealth']);
-    Route::get('/category/home-care',[CategoryController::class,'homeCare']);
-    Route::get('/category/baby-kid',[CategoryController::class,'babykid']);
-});
+// Route::prefix('products')->group(function(){
+//     Route::get('/category/food-beverage',[CategoryController::class,'foodBeverage']);
+//     Route::get('/category/beauty-health',[CategoryController::class,'beautyHealth']);
+//     Route::get('/category/home-care',[CategoryController::class,'homeCare']);
+//     Route::get('/category/baby-kid',[CategoryController::class,'babykid']);
+// });
 // Route::prefix('users')->group(function(){
 //     Route::get('/user/{id}/name/{name}',[UserController::class,'user']);
 //     Route::get('/transactionid/{id}/namaproduct/{product}',[UserController::class,'transaction']);
@@ -37,15 +37,26 @@ Route::prefix('products')->group(function(){
 //Route::get('/user', [UserController::class, 'index']);
 
 // Praktikum 2.6
-Route::get('/user/tambah',[UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan',[UserController::class, 'tambah_simpan']);
+// Route::get('/user/tambah',[UserController::class, 'tambah']);
+// Route::post('/user/tambah_simpan',[UserController::class, 'tambah_simpan']);
 
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 
 
 Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']); // menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']); // menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']); // menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']); // menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']); // menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']); // menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+});
 
 
 
